dc73fa8eb0683776699319b6d6f1b24fe06a57f2
nkorrapa <neha.korrapati@gmail.com>
2017-11-05 08:57:42 -0500

    uploading bot

---
+++ twitter/config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  consumer_key:         '8xTWImKLOXvgGNNJ07fBGTnUZ',
+  consumer_secret:      'kIjfRK5Y47IhssOLUxyUYcB7qvJB0xOf0exoWmwqwAMzk0xpit',
+  access_token:         '926893912819752960-wEKo4xVP5OvMqoRh8kNUfCe18n8tR0i',
+  access_token_secret:  'QpaP3PiMY4VaUQLy0ZJoKpXcNN2va66wDJqzcQ1moQheq',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
